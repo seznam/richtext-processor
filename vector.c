547be32bcd68e70370e8353cd144571686272df0
Martin Jurča <martin.jurca@firma.seznam.cz>
2023-01-28 19:17:52 +0100

    Add vector implementation

---
+++ vector.c
@@ -0,0 +1,186 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include "vector.h"
+
+Vector *Vector_new(itemSize, length, capacity)
+size_t itemSize;
+unsigned long length;
+unsigned long capacity;
+{
+	Vector *vector;
+
+	if (capacity < length) {
+		return NULL;
+	}
+
+	vector = (Vector *) malloc(sizeof(Vector));
+	if (vector == NULL) {
+		return NULL;
+	}
+
+	vector->size.itemSize = itemSize;
+	vector->size.length = length;
+	vector->size.capacity = capacity;
+	vector->items = malloc(itemSize * capacity);
+	if (vector->items == NULL) {
+		free(vector);
+		return NULL;
+	}
+
+	return vector;
+}
+
+Vector *Vector_grow(vector, capacity)
+Vector *vector;
+unsigned long capacity;
+{
+	void *newItems;
+
+	if (vector == NULL) {
+		return NULL;
+	}
+	if (vector->size.capacity <= capacity) {
+		return vector;
+	}
+
+	newItems = realloc(vector->items, vector->size.itemSize * capacity);
+	if (newItems == NULL) {
+		return NULL;
+	}
+
+	vector->items = newItems;
+	vector->size.capacity = capacity;
+	return vector;
+}
+
+Vector *Vector_append(vector, value)
+Vector *vector;
+void *value;
+{
+	unsigned long currentCapacity;
+	unsigned long grownCapacity;
+	char *bucket;
+
+	if (vector == NULL || value == NULL) {
+		return NULL;
+	}
+
+	currentCapacity = vector->size.capacity;
+	if (vector->size.length == currentCapacity) {
+		grownCapacity =
+		    currentCapacity >
+		    VECTOR_AUTO_GROW_LIMIT /
+		    VECTOR_AUTO_GROW_FACTOR ? VECTOR_AUTO_GROW_LIMIT :
+		    currentCapacity * VECTOR_AUTO_GROW_FACTOR;
+		vector = Vector_grow(vector, grownCapacity);
+		if (vector == NULL) {
+			return NULL;
+		}
+	}
+
+	bucket =
+	    (char *)vector->items + vector->size.itemSize * vector->size.length;
+	memcpy(bucket, value, vector->size.itemSize);
+
+	return vector;
+}
+
+Vector *Vector_pop(vector, removedValue)
+Vector *vector;
+void *removedValue;
+{
+	char *bucket;
+
+	if (vector == NULL || !vector->size.length || removedValue == NULL) {
+		return NULL;
+	}
+
+	vector->size.length--;
+	bucket =
+	    (char *)vector->items + vector->size.itemSize * vector->size.length;
+	memcpy(removedValue, bucket, vector->size.itemSize);
+
+	return vector;
+}
+
+Vector *Vector_bigSlice(vector, from, to)
+Vector *vector;
+unsigned long from;
+unsigned long to;
+{
+	Vector *slice;
+	char *sliceDataSrc;
+	unsigned long length;
+
+	if (vector == NULL || from > to) {
+		return NULL;
+	}
+
+	from = from < vector->size.length ? from : vector->size.length;
+	to = to < vector->size.length ? to : vector->size.length;
+	length = to - from;
+
+	slice = Vector_new(vector->size.itemSize, length, length);
+	sliceDataSrc = (char *)vector->items + vector->size.itemSize * from;
+	memcpy(slice->items, sliceDataSrc, vector->size.itemSize * length);
+
+	return slice;
+}
+
+Vector *Vector_slice(vector, from, to)
+Vector *vector;
+long from;
+long to;
+{
+	unsigned long vectorLength;
+	unsigned long adjustedFrom;
+	unsigned long adjustedTo;
+
+	if (vector == NULL) {
+		return NULL;
+	}
+
+	vectorLength = vector->size.length;
+	adjustedFrom =
+	    from >=
+	    0 ? (unsigned long)from : vectorLength - (-from % vectorLength);
+	adjustedTo =
+	    to >= 0 ? (unsigned long)to : vectorLength - (-to % vectorLength);
+
+	return Vector_bigSlice(vector, adjustedFrom, adjustedTo);
+}
+
+Vector *Vector_get(vector, index, value)
+Vector *vector;
+unsigned long index;
+void *value;
+{
+	char *bucket;
+
+	if (vector == NULL || index >= vector->size.length || value == NULL) {
+		return NULL;
+	}
+
+	bucket = (char *)vector->items + vector->size.itemSize * index;
+	memcpy(value, bucket, vector->size.itemSize);
+
+	return vector;
+}
+
+Vector *Vector_set(vector, index, value)
+Vector *vector;
+unsigned long index;
+void *value;
+{
+	char *bucket;
+
+	if (vector == NULL || index >= vector->size.length || value == NULL) {
+		return NULL;
+	}
+
+	bucket = (char *)vector->items + vector->size.itemSize * index;
+	memcpy(bucket, value, vector->size.itemSize);
+
+	return vector;
+}
---
+++ vector.h
@@ -0,0 +1,59 @@
+/*
+ * Vector implementation, compatible with ANSI C (C89) and traditional
+ * compilers.
+ *
+ * There are many other existing implementation that were rejected because they
+ * target newer C standard or compilers (there is no point having a project
+ * targeting older C versions if its dependencies cannot be built on the target
+ * compilers):
+ *  - vc_vector - https://github.com/skogorev/vc_vector - targets C99
+ *  - GArray from GTK glib -
+ *    https://gitlab.gnome.org/GNOME/glib/-/tree/main/glib - does not target
+ *    traditional compilers.
+ *  - dynarray -
+ *    https://stackoverflow.com/questions/3456446/a-good-c-equivalent-of-stl-vector#answer-3463498 -
+ *    does not target traditional compilers.
+ *  - SGLIB - https://sourceforge.net/projects/sglib/ - targets newer C
+ *    versions.
+ *  - qLibc - https://github.com/wolkykim/qlibc - targets newer C versions.
+ *  - Gena - https://github.com/cher-nov/Gena - does not target traditional
+ *    compilers.
+ */
+
+#ifndef VECTOR_HEADER_FILE
+#define VECTOR_HEADER_FILE 1
+
+#include <stddef.h>
+
+typedef struct VectorSize {
+	size_t itemSize;
+	unsigned long length;
+	unsigned long capacity;
+} VectorSize;
+
+typedef struct Vector {
+	VectorSize size;
+	void *items;
+} Vector;
+
+static const unsigned long VECTOR_AUTO_GROW_FACTOR = 2;
+static const unsigned long VECTOR_AUTO_GROW_LIMIT = 64;
+
+Vector *Vector_new(size_t itemSize,
+		   unsigned long length, unsigned long capacity);
+
+Vector *Vector_grow(Vector * vector, unsigned long capacity);
+
+Vector *Vector_append(Vector * vector, void *value);
+
+Vector *Vector_pop(Vector * vector, void *removedValue);
+
+Vector *Vector_bigSlice(Vector * vector, unsigned long from, unsigned long to);
+
+Vector *Vector_slice(Vector * vector, long from, long to);
+
+Vector *Vector_get(Vector * vector, unsigned long index, void *value);
+
+Vector *Vector_set(Vector * vector, unsigned long index, void *value);
+
+#endif
