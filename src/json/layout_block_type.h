736a44f460e6e563f590592f224050d933bde674
Martin Jurča <martin.jurca@firma.seznam.cz>
2023-06-22 15:51:09 +0200

    Fix code style

--- src/json/layout_block_type.h
+++ src/json/layout_block_type.h
@@ -1,8 +1,8 @@
 #ifndef JSON_LAYOUT_BLOCK_TYPE_HEADER_FILE
 #define JSON_LAYOUT_BLOCK_TYPE_HEADER_FILE 1
 
-#include "json_value.h"
 #include "../layout_block_type.h"
+#include "json_value.h"
 
 JSONValue *LayoutBlockType_toJSON(LayoutBlockType type);
 
