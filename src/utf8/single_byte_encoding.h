16970a4cb620ce8683d2db9c43471bad61e1c2ef
Martin Jurča <martin.jurca@firma.seznam.cz>
2023-07-10 15:27:10 +0200

    Fix a typo

--- src/utf8/single_byte_encoding.h
+++ src/utf8/single_byte_encoding.h
@@ -3,21 +3,21 @@
 
 typedef enum SingleByteEncoding {
 	SingleByteEncoding_US_ASCII,
-	SingleByteEncoding_IS_8859_1,
-	SingleByteEncoding_IS_8859_2,
-	SingleByteEncoding_IS_8859_3,
-	SingleByteEncoding_IS_8859_4,
-	SingleByteEncoding_IS_8859_5,
-	SingleByteEncoding_IS_8859_6,
-	SingleByteEncoding_IS_8859_7,
-	SingleByteEncoding_IS_8859_8,
-	SingleByteEncoding_IS_8859_9,
-	SingleByteEncoding_IS_8859_10,
-	SingleByteEncoding_IS_8859_11,
-	SingleByteEncoding_IS_8859_13,
-	SingleByteEncoding_IS_8859_14,
-	SingleByteEncoding_IS_8859_15,
-	SingleByteEncoding_IS_8859_16
+	SingleByteEncoding_ISO_8859_1,
+	SingleByteEncoding_ISO_8859_2,
+	SingleByteEncoding_ISO_8859_3,
+	SingleByteEncoding_ISO_8859_4,
+	SingleByteEncoding_ISO_8859_5,
+	SingleByteEncoding_ISO_8859_6,
+	SingleByteEncoding_ISO_8859_7,
+	SingleByteEncoding_ISO_8859_8,
+	SingleByteEncoding_ISO_8859_9,
+	SingleByteEncoding_ISO_8859_10,
+	SingleByteEncoding_ISO_8859_11,
+	SingleByteEncoding_ISO_8859_13,
+	SingleByteEncoding_ISO_8859_14,
+	SingleByteEncoding_ISO_8859_15,
+	SingleByteEncoding_ISO_8859_16
 } SingleByteEncoding;
 
 #endif
--- src/utf8/single_byte_to_utf8.c
+++ src/utf8/single_byte_to_utf8.c
@@ -29,35 +29,35 @@ string *input;
 	switch (inputEncoding) {
 	case SingleByteEncoding_US_ASCII:
 		return usAsciiToUtf8(input);
-	case SingleByteEncoding_IS_8859_1:
+	case SingleByteEncoding_ISO_8859_1:
 		return iso88591ToUtf8(input);
-	case SingleByteEncoding_IS_8859_2:
+	case SingleByteEncoding_ISO_8859_2:
 		return iso88592ToUtf8(input);
-	case SingleByteEncoding_IS_8859_3:
+	case SingleByteEncoding_ISO_8859_3:
 		return iso88593ToUtf8(input);
-	case SingleByteEncoding_IS_8859_4:
+	case SingleByteEncoding_ISO_8859_4:
 		return iso88594ToUtf8(input);
-	case SingleByteEncoding_IS_8859_5:
+	case SingleByteEncoding_ISO_8859_5:
 		return iso88595ToUtf8(input);
-	case SingleByteEncoding_IS_8859_6:
+	case SingleByteEncoding_ISO_8859_6:
 		return iso88596ToUtf8(input);
-	case SingleByteEncoding_IS_8859_7:
+	case SingleByteEncoding_ISO_8859_7:
 		return iso88597ToUtf8(input);
-	case SingleByteEncoding_IS_8859_8:
+	case SingleByteEncoding_ISO_8859_8:
 		return iso88598ToUtf8(input);
-	case SingleByteEncoding_IS_8859_9:
+	case SingleByteEncoding_ISO_8859_9:
 		return iso88599ToUtf8(input);
-	case SingleByteEncoding_IS_8859_10:
+	case SingleByteEncoding_ISO_8859_10:
 		return iso885910ToUtf8(input);
-	case SingleByteEncoding_IS_8859_11:
+	case SingleByteEncoding_ISO_8859_11:
 		return iso885911ToUtf8(input);
-	case SingleByteEncoding_IS_8859_13:
+	case SingleByteEncoding_ISO_8859_13:
 		return iso885913ToUtf8(input);
-	case SingleByteEncoding_IS_8859_14:
+	case SingleByteEncoding_ISO_8859_14:
 		return iso885914ToUtf8(input);
-	case SingleByteEncoding_IS_8859_15:
+	case SingleByteEncoding_ISO_8859_15:
 		return iso885915ToUtf8(input);
-	case SingleByteEncoding_IS_8859_16:
+	case SingleByteEncoding_ISO_8859_16:
 		return iso885916ToUtf8(input);
 	default:
 		return NULL;
